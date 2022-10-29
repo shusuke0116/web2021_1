2aa74ebd7b39f4b28aa5eac83684a9f78a45b50f
柊介 <104350056+shusuke0116@users.noreply.github.com>
2022-10-29 00:45:09 +0000

    表示方法の変更

--- insert_pokemon.js
+++ insert_pokemon.js
@@ -2,11 +2,14 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test2.db');
 
 let sqls =[
+/*
   `insert into pokemon ("number","name","attack","defence","hp","type1_id","type2_id") values ("618","マッギョ（ガラル）","144","171","240","9","17");`,
   `insert into pokemon ("number","name","attack","defence","hp","type1_id","type2_id") values ("302","ヤミラミ","141","136","137","16","14");`,
   `insert into pokemon ("number","name","attack","defence","hp","type1_id","type2_id") values ("308","チャーレム","121","152","155","7","11");`,
   `insert into pokemon ("number","name","attack","defence","hp","type1_id","type2_id") values ("709","オーロット","201","154","198","14","5");`,
   `insert into pokemon ("number","name","attack","defence","hp","type1_id","type2_id") values ("031","ニドクイン","180","173","207","8","9");`,
+*/
+  `insert into pokemon ("number","name","attack","defence","hp","type1_id") values ("379","レジスチル","143","285","190","17");`,
 ]
 
 for(let sql of sqls){
--- select.js
+++ select.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test2.db');
 
 let sql = `
-select p.number,p.name,type.name as type1,p.type2 from (select pokemon.number,pokemon.name,pokemon.type1_id,type.name as type2 from pokemon inner join type on pokemon.type2_id = type.id) as p inner join type on p.type1_id = type.id;
+select p.number,p.name,type.name as type1,p.type2 from (select pokemon.number,pokemon.name,pokemon.type1_id,type.name as type2 from pokemon left join type on pokemon.type2_id = type.id) as p inner join type on p.type1_id = type.id;
 `
 
 db.serialize( () => {
@@ -12,7 +12,11 @@ db.serialize( () => {
 			return;
 		}
 		for( let data of row ) {
-			console.log('No.' + data.number + ' : ' + data.name + ' : ' + data.type1 + ' ・ ' + data.type2);
+      if(data.type2 != null){
+        console.log('No.' + data.number + ' : ' + data.name + ' : ' + data.type1 + '・' + data.type2);
+      } else{
+        console.log('No.' + data.number + ' : ' + data.name + ' : ' + data.type1);
+      }
 		}
 	});
 });
