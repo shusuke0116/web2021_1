311cf191c0af4d8c75645806a483109ac615ca20
柊介 <104350056+shusuke0116@users.noreply.github.com>
2022-10-29 01:13:33 +0000

    データの追加

--- select.js
+++ select.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test2.db');
 
 let sql = `
-select p.number,p.name,type.name as type1,p.type2 from (select pokemon.number,pokemon.name,pokemon.type1_id,type.name as type2 from pokemon left join type on pokemon.type2_id = type.id) as p inner join type on p.type1_id = type.id;
+select p.id,p.number,p.name,type.name as type1,p.type2 from (select pokemon.id,pokemon.number,pokemon.name,pokemon.type1_id,type.name as type2 from pokemon left join type on pokemon.type2_id = type.id) as p inner join type on p.type1_id = type.id;
 `
 
 db.serialize( () => {
@@ -13,9 +13,10 @@ db.serialize( () => {
 		}
 		for( let data of row ) {
       if(data.type2 != null){
-        console.log('No.' + data.number + ' : ' + data.name + ' : ' + data.type1 + '・' + data.type2);
+        console.log(data.id + ' : ' + 
+ 'No.' + data.number + ' : ' + data.name + ' : ' + data.type1 + '・' + data.type2);
       } else{
-        console.log('No.' + data.number + ' : ' + data.name + ' : ' + data.type1);
+        console.log(data.id + ' : ' + 'No.' + data.number + ' : ' + data.name + ' : ' + data.type1);
       }
 		}
 	});
