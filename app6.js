f99d5ebc40e7ed80176b667c0e1c434805b15bbc
柊介 <104350056+shusuke0116@users.noreply.github.com>
2022-11-04 08:33:44 +0000

    ページの追加

--- app6.js
+++ app6.js
@@ -2,31 +2,28 @@ const express = require("express");
 const app = express();
 
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('test.db');
+const db = new sqlite3.Database('test2.db');
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  const message = "Hello world";
+  const message = "top page";
   res.render('show', {mes:message});
 });
 
-app.get("/db", (req, res) => {
-    db.serialize( () => {
-        db.all("select id, 都道府県, 人口 from example;", (error, row) => {
-            if( error ) {
-                res.render('show', {mes:"エラーです"});
-            }
-            res.render('select', {data:row});
-        })
-    })
-})
-app.get("/top", (req, res) => {
+app.get("/pokemon", (req, res) => {
     //console.log(req.query.pop);    // ①
-    let desc = "";
-    if( req.query.desc ) desc = " desc";
-    let sql = "select id, 都道府県, 人口 from example order by 人口" + desc + " limit " + req.query.pop + ";";
+    if( req.query.order ) order = " order by p.name";
+    else order = " order by p.number";
+    if( req.query.desc ) desc = " desc";  
+    else desc = "";
+    if( req.query.pop ) pop = " limit " + req.query.pop;
+    else pop = "";
+    if( req.query.type ) type = " where type2 = '" + req.query.type + "' or type1 = '" + req.query.type + "'";  
+    else type = "";
+    
+    let sql = "select p.id,p.number,p.name,type.name as type1,p.type2 from (select pokemon.id,pokemon.number,pokemon.name,pokemon.type1_id,type.name as type2 from pokemon left join type on pokemon.type2_id = type.id) as p inner join type on p.type1_id = type.id" + type　+ order + desc + pop + ";";
     //console.log(sql);    // ②
     db.serialize( () => {
         db.all(sql, (error, data) => {
@@ -34,10 +31,31 @@ app.get("/top", (req, res) => {
                 res.render('show', {mes:"エラーです"});
             }
             //console.log(data);    // ③
-            res.render('select', {data:data});
+            res.render('pokemon', {data:data});
         })
     })
 })
+
+app.get("/type", (req, res) => {
+    //console.log(req.query.pop);    // ①
+    let desc = "";
+    if( req.query.desc ) desc = " desc";
+    let sql = "select name from type" + desc + ";";
+    //console.log(sql);    // ②
+    db.serialize( () => {
+        db.all(sql, (error, data) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            //console.log(data);    // ③
+            res.render('type', {data:data});
+        })
+    })
+})
+
+
+
+
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
 });
